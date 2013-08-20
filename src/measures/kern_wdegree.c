2133b23da5b02120b8219a7ad6c50bdccfdfe416
Konrad Rieck <konrad@mlsec.org>
2013-08-20 21:30:46 +0200

    closed form of weighted-degree kernel

--- src/measures/kern_wdegree.c
+++ src/measures/kern_wdegree.c
@@ -26,8 +26,8 @@
 extern config_t cfg;
 
 /* Local variables */
-int degree = 3;		/**< Degree of kernel */
-int shift = 0;		/**< Shift of kernel */
+int degree = 3;         /**< Degree of kernel */
+int shift = 0;          /**< Shift of kernel */
 
 /**
  * Initializes the similarity measure
@@ -38,16 +38,42 @@ void kern_wdegree_config()
     config_lookup_int(&cfg, "measures.kern_wdegree.shift", &shift);
 }
 
+/**
+ * Weighting function for matching blocks. 
+ * Sonnenburg, Raetsch, Schoelkopf: Large scale genomic sequence SVM
+ * classifiers.  ICML 2005: 848-855
+ * @param len length of block
+ * @param degree of kernel
+ * @return weighting
+ */
+static float weight(float len, int degree)
+{
+    assert(len > 0 && degree > 0);
+
+    if (len <= degree) {
+        float w = len * (-len * len + 3 * degree * len + 3 * degree + 1);
+        return w / (3 * degree * (degree + 1));
+    } else {
+        return (3 * len - degree + 1) / 3;
+    }
+}
+
+/**
+ * Implementation of weighted-degree kernel in block mode.
+ * @param x Symbols of string x
+ * @param y Symbols of string y
+ * @param len Length of region to match
+ * @return kernel value
+ */
 static float kern_wdegree(sym_t *x, sym_t *y, int len)
 {
     int i, start;
-    float n = degree * (degree + 1.0);    
     float k = 0;
 
-    for (i = 0, start = -1; i < len ; i++) {
+    for (i = 0, start = -1; i < len; i++) {
         /* Identify matching region */
         if (x[i] == y[i]) {
-            if (start == -1) 
+            if (start == -1)
                 start = i;
             continue;
         }
@@ -55,49 +81,37 @@ static float kern_wdegree(sym_t *x, sym_t *y, int len)
         /* No match found continue */
         if (start == -1)
             continue;
-        
-        /* FIXME! There is a closed form by Sonnenburg. Too lazy now */
-        len = i - start;    
-        for (int j = degree; j > 0; j--) { 
-            if (j > len)
-                continue;
-            k += 2 * (len - j + 1) * (degree - j + 1.0) / n;
-        }
+
+        k += weight(i - start, degree);
         start = -1;
     }
-    
-    if (start != -1) {
-        len = i - start;           
-        for (int j = degree; j > 0; j--) { 
-            if (j > len)
-                continue;
-            k += 2 * (len - j + 1) * (degree - j + 1.0) / n;
-        }       
-    }
-    
+
+    if (start != -1)
+        k += weight(i - start, degree);
+
     return k;
 }
 
 /**
- * Compute the weighted degree kernel for strings. If the strings 
- * have unequal size, the remaining symbols of the longer string are
- * ignored (in accordance with the kernel definition)
+ * Compute the weighted-degree kernel with shift. If the strings have
+ * unequal size, the remaining symbols of the longer string are ignored (in
+ * accordance with the kernel definition)
  * @param x first string 
  * @param y second string
- * @return weighted degree kernel
+ * @return weighted-degree kernel
  */
 float kern_wdegree_compare(str_t x, str_t y)
 {
     float k = 0;
     int s, len;
 
-    /* Loop over strings */
+    /* Loop over shifts */
     for (s = -shift; s <= shift; s++) {
         if (s <= 0) {
             len = fmax(fmin(x.len, y.len + s), 0);
             k += kern_wdegree(x.str.s, y.str.s - s, len);
         } else {
-            len = fmax(fmin(x.len - s, y.len), 0);        
+            len = fmax(fmin(x.len - s, y.len), 0);
             k += kern_wdegree(x.str.s + s, y.str.s, len);
         }
     }
