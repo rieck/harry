353836f0014af9ba413cfd28c569471129abbd8c
Konrad Rieck <konrad@mlsec.org>
2013-09-15 19:58:45 +0200

    first code for rwlock

---
+++ src/rwlock.c
@@ -0,0 +1,99 @@
+/*
+ * Harry - A Tool for Measuring String Similarity
+ * Copyright (C) 2013 Konrad Rieck (konrad@mlsec.org)
+ * --
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.  This program is distributed without any
+ * warranty. See the GNU General Public License for more details. 
+ */
+
+#include "config.h"
+#include "common.h"
+#include "rwlock.h"
+#include "util.h"
+
+/**
+ * @defgroup rwlock Read-write lock for OpenMP. 
+ * Code adapted from http://www.linux.org.ru/forum/development/4510260
+ * Two OpenMP locks are used which enables having totally independent 
+ * rwlocks in different contexts.
+ * @author Konrad Rieck (konrad@mlsec.org)
+ * @{
+ */
+
+/**
+ * Init a read-write lock.
+ * @param rw pointer to lock structure
+ */
+void rwlock_init(rwlock_t *rw)
+{
+    omp_init_lock(&rw->read);
+    omp_init_lock(&rw->write);
+    rw->readers = 0;
+}
+/**
+ * Destroy a read-write lock.
+ * @param rw pointer to lock structure
+ */
+void rwlock_destroy(rwlock_t *rw)
+{
+    omp_destroy_lock(&rw->read);
+    omp_destroy_lock(&rw->write);
+}
+
+/**
+ * Set lock for reading. 
+ * @param rw pointer to lock structure
+ */
+void rwlock_set_rlock(rwlock_t *rw)
+{
+    omp_set_lock(&rw->read);
+    
+    if(rw->readers == 0)
+		omp_set_lock(&rw->write);
+    
+    rw->readers++;
+
+    omp_unset_lock(&rw->read);
+}
+
+/**
+ * Unset lock for reading. 
+ * @param rw pointer to lock structure
+ */
+void rwlock_unset_rlock(rwlock_t *rw)
+{
+    omp_set_lock(&rw->read);
+
+    assert(rw->readers > 0);
+    rw->readers--;
+    
+    if (rw->readers == 0)
+        omp_unset_lock(&rw->write);
+    
+    omp_unset_lock(&rw->read);
+}   
+
+/**
+ * Set lock for writing. 
+ * @param rw pointer to lock structure
+ */
+void rwlock_set_wlock(rwlock_t *rw)
+{
+    omp_set_lock(&rw->write);
+	assert(rw->readers == 0);
+}
+
+/**
+ * Unset lock for writing.
+ * @param rw pointer to lock structure
+ */
+void rwlock_unset_wlock(rwlock_t *rw)
+{
+    assert(rw->readers == 0);
+	omp_unset_lock(&rw->write);
+}
+
+/** @} */
---
+++ src/rwlock.h
@@ -0,0 +1,30 @@
+/*
+ * Harry - A Tool for Measuring String Similarity
+ * Copyright (C) 2013 Konrad Rieck (konrad@mlsec.org)
+ * --
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.  This program is distributed without any
+ * warranty. See the GNU General Public License for more details. 
+ */
+
+#ifndef RWLOCK_H
+#define RWLOCK_H
+
+#include <omp.h>
+
+typedef struct {
+    omp_lock_t write;   /**< Write lock */
+    omp_lock_t read;    /**< Read lock */
+    int readers;        /**< Number of readers */
+} rwlock_t;
+
+void rwlock_init(rwlock_t *rw);
+void rwlock_destroy(rwlock_t *rw);
+void rwlock_set_rlock(rwlock_t *rw);
+void rwlock_unset_rlock(rwlock_t *rw);
+void rwlock_set_wlock(rwlock_t *rw);
+void rwlock_unset_wlock(rwlock_t *rw);
+
+#endif
