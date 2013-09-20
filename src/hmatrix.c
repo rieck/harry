43994fbaf942f17cfbbfa476169322489f13fe12
Konrad Rieck <konrad@mlsec.org>
2013-09-20 19:30:03 +0200

    sync

---
+++ src/hmatrix.c
@@ -0,0 +1,30 @@
+/*
+ * Harry - A Tool for Measuring String Similarity
+ * Copyright (C) 2013 Konrad Rieck (konrad@mlsec.org)
+ * --
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.  This program is distributed without any
+ * warranty. See the GNU General Public License for more details. 
+ */
+
+/**
+ * @defgroup matrix Matrix functions
+ * Functions for processing similarity values in a matrix
+ * @author Konrad Rieck (konrad@mlsec.org)
+ * @{
+ */
+
+#include "config.h"
+#include "common.h"
+#include "util.h"
+#include "hstring.h"
+#include "murmur.h"
+
+/* External variable */
+extern int verbose;
+extern config_t cfg;
+
+
+/** @} */
