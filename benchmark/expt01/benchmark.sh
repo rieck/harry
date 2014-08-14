7a925da367942a9659469e575a048292aaaf59a9
Konrad Rieck <konrad@mlsec.org>
2014-08-14 19:39:52 +0200

    changed looping in benchmark

--- benchmark/expt01/benchmark.sh
+++ benchmark/expt01/benchmark.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-datasets="enron hids nids reuters sentences sprot arts"
-measures="hamming"
-nthreads="1 2 3 4 5 6 7 9 10 12 "
+datasets="enron hids nids reuters sentences sprot" # arts
+measures="hamming levenshtein"
+nthreads="1 2 3 4 5 6 7 8"
 runs="1 2 3"
 
 export TIMEFORMAT="%R %U %S" 
 
-for d in $datasets ; do
-    for m in $measures ; do
-        for n in $nthreads ; do
-            for r in $runs ; do
+for r in $runs ; do
+    for d in $datasets ; do
+        for m in $measures ; do
+            for n in $nthreads ; do
                 printf "$d $m $n $r " 1>&2
                 time ../../src/harry -n $n -m $m -c ../$d.cfg \
                      ../$d.t*.gz /dev/null 
