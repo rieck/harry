aefdbb1378f806573a04c5c8cb5d36ea9e183b34
Konrad Rieck <konrad@mlsec.org>
2014-08-14 19:24:54 +0200

    revived benchmark code

--- benchmark/expt01/benchmark.sh
+++ benchmark/expt01/benchmark.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-datasets="enron hids nids reuters sentences sprot" # arts
-measures="hamming levenshtein"
-gcache="0 1"
-nthreads="1 2 4 8"
+datasets="enron hids nids reuters sentences sprot arts"
+measures="hamming"
+nthreads="1 2 3 4 5 6 7 9 10 12 "
 runs="1 2 3"
 
 export TIMEFORMAT="%R %U %S" 
@@ -11,12 +10,10 @@ export TIMEFORMAT="%R %U %S"
 for d in $datasets ; do
     for m in $measures ; do
         for n in $nthreads ; do
-            for g in $gcache ; do
-                for r in $runs ; do
-                    printf "$d $m $n $g $r " 1>&2
-                    time ../../src/harry -n $n -g $g -m $m -c ../$d.cfg \
-                         ../$d.t*.gz /dev/null 
-                done
+            for r in $runs ; do
+                printf "$d $m $n $r " 1>&2
+                time ../../src/harry -n $n -m $m -c ../$d.cfg \
+                     ../$d.t*.gz /dev/null 
             done
         done
     done
