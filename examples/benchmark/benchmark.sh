96760afd6902ec7ffee96fa68f4676eaa150f41d
Konrad Rieck <konrad@mlsec.org>
2014-08-16 19:05:20 +0200

    direct output

--- examples/benchmark/benchmark.sh
+++ examples/benchmark/benchmark.sh
@@ -2,10 +2,9 @@
 
 DATASETS="enron hids nids reuters sentences sprot"
 MEASURES="levenshtein"
-THREADS="1 2 3 4 5 6 7 8"
+THREADS="4 1 2 3 5 6 7 8"
 RUNS="1 2 3"
-RANGE="-x :100"
-LOGFILE=`hostname -s`-`date "+%Y%m%d%H%M"`.log
+RANGE=""
 
 export TIMEFORMAT="%R %U %S" 
 
@@ -13,9 +12,9 @@ for D in $DATASETS ; do
     for M in $MEASURES ; do
         for T in $THREADS ; do
             for R in $RUNS ; do
-                printf "$D $M $T $R " 1>&2 >> $LOGFILE
+                printf "$D $M $T $R " 1>&2 
                 time ../../src/harry -n $T -m $M -o null $range \
-                     -c $D.cfg $D.t*.gz /dev/null 1>&2 >> $LOGFILE
+                     -c $D.cfg $D.t*.gz /dev/null 1>&2
             done
         done
     done
