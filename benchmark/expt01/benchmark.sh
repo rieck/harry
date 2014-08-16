ce30aee46e7ab93499c668b0b8d0fa95535e8604
Konrad Rieck <konrad@mlsec.org>
2014-08-16 15:41:40 +0200

    the ARTS dataset is too different from the others and spoils any experiment :/

--- benchmark/expt01/benchmark.sh
+++ benchmark/expt01/benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-datasets="enron" # hids nids reuters sentences sprot" # arts
-measures="levenshtein" # compression hamming"
+datasets="enron hids nids reuters sentences sprot"
+measures="levenshtein compression hamming"
 nthreads="1 2 3 4 5 6 7 8"
 runs="1 2 3"
 
@@ -12,7 +12,7 @@ for r in $runs ; do
         for m in $measures ; do
             for n in $nthreads ; do
                 printf "$d $m $n $r " 1>&2
-                time ../../src/harry -n $n -m $m -c ../$d.cfg \
+                time ../../src/harry -x 000:100 -y 100:200 -n $n -m $m -c ../$d.cfg \
                      ../$d.t*.gz /dev/null 
             done
         done
