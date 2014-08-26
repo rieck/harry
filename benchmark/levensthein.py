f293ebce3b93e6f7f5b7b7e94bffc6369cdb66ef
Konrad Rieck <konrad@mlsec.org>
2014-08-26 15:46:10 +0200

    added benchmark script for py-levenshtein

---
+++ benchmark/levensthein.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Simple code for benchmarking python-Levenshtein 0.11.2
+
+import sys
+import gzip
+import random
+import Levenshtein
+import time
+
+# Load data in input mode 'line' 
+data = gzip.open(sys.argv[1]).read().splitlines()
+runtime = int(sys.argv[2])
+
+start = time.time()
+counter = 0
+
+while time.time() - start < runtime:
+    # Compute Levenshtein distance for random pair
+    i = random.randint(0, len(data) - 1)
+    j = random.randint(0, len(data) - 1)
+    Levenshtein.distance(data[i], data[j])
+    counter += 1
+    
+print '%d comparisons; %d seconds; %d threads;' % (counter, runtime, 1)
+    
