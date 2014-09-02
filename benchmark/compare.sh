935ca1861e20d5e888da81de950393e3bb786d86
Konrad Rieck <konrad@mlsec.org>
2014-09-02 12:52:03 +0200

    changes to comparison script

--- benchmark/compare.sh
+++ benchmark/compare.sh
@@ -1,33 +1,42 @@
 #!/bin/sh
 
-DATASETS="webfp tweets sprot"
+DATASETS="arts sprot tweets webfp http"
 DATADIR="/data/projects/harry/data"
-MEASURES="levenshtein jarowinkler"
+RUNS="1 2 3 4 5"
 
 export OMP_NUM_THREADS=1
+for R in $RUNS ; do
+    for DATA in $DATASETS ; do
+        gunzip -c $DATADIR/$DATA.txt.gz | shuf | head -100 > data.txt
 
-for DATA in $DATASETS ; do
-    gunzip -c $DATADIR/$DATA.txt.gz | head -100 > data.txt
+        printf "shogun $DATA subsequence "
+        time -o /tmp/log -f "%e %U %S" python pyshogun.py data.txt
+        cat /tmp/log
+        printf "harry $DATA subsequence "
+        time -o /tmp/log -f  "%e %U %S" ../src/harry -d '' -m subsequence -n 1 data.txt /dev/null 
+        cat /tmp/log        
 
-    printf "shogun $DATA subsequence "
-    time -f "%e %U %S" python pyshogun.py data.txt
-    printf "harry $DATA subsequence "
-    time -f  "%e %U %S" ../src/harry -d '' -m subsequence -n 1 data.txt /dev/null
+        #printf "pylevenshtein $DATA jarowinkler "
+        #time -o /tmp/log -f "%e %U %S" python pylevenshtein.py data.txt jarowinkler
+        #cat /tmp/log        
+        #printf "harry $DATA jarowinkler "
+        #time -o /tmp/log -f "%e %U %S" ../src/harry -d '' -m jarowinkler -n 1 data.txt /dev/null
+        #cat /tmp/log        
 
-    printf "pylevenshtein $DATA jarowinkler "
-    time -f "%e %U %S" python pylevenshtein.py data.txt jarowinkler
-    printf "harry $DATA jarowinkler "
-    time -f "%e %U %S" ../src/harry -d '' -m jarowinkler -n 1 data.txt /dev/null
-    
-    printf "complearn $DATA compression "
-    time -f "%e %U %S" ncd -c zlib -t data.txt data.txt > /dev/null
-    printf "harry $DATA compression "
-    time -f "%e %U %S" ../src/harry -d '' -m compression -n 1 data.txt /dev/null
+        printf "complearn $DATA compression "
+        time -o /tmp/log -f "%e %U %S" ncd -c zlib -t data.txt data.txt > /dev/null
+        cat /tmp/log
+        printf "harry $DATA compression "
+        time -o /tmp/log -f "%e %U %S" ../src/harry -d '' -m compression -n 1 data.txt /dev/null
+        cat /tmp/log
 
-    printf "pylevenshtein $DATA levenshtein "
-    time -f "%e %U %S" python pylevenshtein.py data.txt levenshtein
-    printf "harry $DATA levenshtein "
-    time -f "%e %U %S" ../src/harry -d '' -m levenshtein -n 1 data.txt /dev/null
+        printf "pylevenshtein $DATA levenshtein "
+        time -o /tmp/log -f "%e %U %S" python pylevenshtein.py data.txt levenshtein
+        cat /tmp/log
+        printf "harry $DATA levenshtein "
+        time -o /tmp/log -f "%e %U %S" ../src/harry -d '' -m levenshtein -n 1 data.txt /dev/null
+        cat /tmp/log
 
-    rm data.txt        
+        rm data.txt        
+    done
 done
