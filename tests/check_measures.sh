2792e168ae7c9f30776201015cd1a6e8382979a7
Konrad Rieck <konrad@mlsec.org>
2014-10-31 00:21:54 +0100

    added new test case

---
+++ tests/check_measures.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Simple test comparing precomputed output for all similarity measures
+# against compiled version of Harry
+# 
+
+DATA=$SRCDIR/tests/data1.txt
+CHECK=$SRCDIR/tests/check_measures.txt
+HARRY=$BUILDDIR/src/harry
+OUTPUT=$TMPDIR/harry-$$.txt
+
+rm -f $OUTPUT
+for M in dist_bag dist_compression dist_damerau dist_hamming dist_jaro \
+         dist_jarowinkler dist_kernel dist_lee dist_levenshtein dist_osa \
+         kern_distance kern_spectrum kern_subsequence kern_wdegree \
+         sim_braun sim_dice sim_jaccard sim_kulczynski sim_otsuka \
+         sim_simpson sim_sokal ; do
+
+    $HARRY -m $M $DATA - | grep -v '#' >> $OUTPUT
+done
+
+diff $OUTPUT $CHECK
+RET=$?
+rm -f $OUTPUT
+exit $RET
