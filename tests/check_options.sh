dfff5db5ab35e4952aa805eacdbee00caeca17f2
Konrad Rieck <konrad@mlsec.org>
2014-10-31 00:33:42 +0100

    test case for options

---
+++ tests/check_options.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Simple test comparing precomputed output for different options
+# against compiled version of Harry
+# 
+
+DATA=$SRCDIR/tests/data1.txt
+CHECK=$SRCDIR/tests/check_options.txt
+HARRY=$BUILDDIR/src/harry
+OUTPUT=$TMPDIR/harry-$$.txt
+
+rm -f $OUTPUT 
+
+for O in "-m hamming" "-x 1:2" "-y 1:" "-d%20%0a%0d" "-s 0:2" \
+         "-d%20 --soundex" "--reverse_str" "--save_indices" \
+         "--save_labels" "--save_sources" "--decode_str" ; do
+    $HARRY $O $DATA - | grep -v -E '^#' >> $OUTPUT
+done
+
+diff $OUTPUT $CHECK
+RET=$?
+rm -f $OUTPUT
+exit $RET
